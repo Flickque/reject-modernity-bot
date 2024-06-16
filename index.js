c6660ff9e23283b23096a6905050996da0ec9889
Flickque <flick86rus1@yandex.ru>
2024-06-16 17:24:39 +0400

    initial commit

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ const domain = process.env.RENDER_EXTERNAL_URL ? process.env.RENDER_EXTERNAL_URL
 
 const dictionaryMap = new Map([
     ['понедельник', {name: 'monday', length: 1}],
-    ['вторник',  {name: 'tuesday', length: 3}],
+    ['вторник',  {name: 'tuesday', length: 2}],
     ['сред',  {name: 'wednesday', length: 3}],
-    ['четверг',  {name: 'thursday', length: 1}],
+    ['четверг',  {name: 'thursday', length: 2}],
     ['пятниц',  {name: 'friday', length: 3}],
     ['суббот',  {name: 'saturday', length: 1}],
     ['воскресень',  {name: 'sunday', length: 2}],
