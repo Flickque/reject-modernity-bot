01d459fc52056f8bd9f6b40b9b1f7e2cdcfa3dbe
Flickque <flick86rus1@yandex.ru>
2024-06-16 17:19:06 +0400

    initial commit

--- index.js
+++ index.js
@@ -7,15 +7,20 @@ const port = 3005;
 const domain = process.env.RENDER_EXTERNAL_URL ? process.env.RENDER_EXTERNAL_URL : 'https://reject-modernity-bot.onrender.com';
 
 const dictionaryMap = new Map([
-    ['понедельник', 'monday'],
-    ['вторник', 'tuesday'],
-    ['сред', 'wednesday'],
-    ['четверг', 'thursday'],
-    ['пятниц', 'friday'],
-    ['суббот', 'saturday'],
-    ['воскресень', 'sunday'],
-    ['праздник', 'holiday'],
+    ['понедельник', {name: 'monday', length: 1}],
+    ['вторник',  {name: 'tuesday', length: 3}],
+    ['сред',  {name: 'wednesday', length: 3}],
+    ['четверг',  {name: 'thursday', length: 1}],
+    ['пятниц',  {name: 'friday', length: 3}],
+    ['суббот',  {name: 'saturday', length: 1}],
+    ['воскресень',  {name: 'sunday', length: 2}],
+    ['праздник',  {name: 'holiday', length: 1}],
 ]);
+
+const getRandomNumber = (n) => {
+    if (!n) return 1
+    return Math.floor(Math.random() * n) + 1;
+}
 
 bot.on('message', (ctx) => {
     const message = ctx.message.text ? ctx?.message?.text?.toLowerCase() : ctx?.message?.caption?.toLowerCase();
@@ -24,7 +29,7 @@ bot.on('message', (ctx) => {
     for (let [key, value] of dictionaryMap.entries()) {
         if (msg.includes(key)) {
             try{
-                const filePath = `public/videos/${value}-1.mp4`;
+                const filePath = `public/videos/${value.name}-${getRandomNumber(value.length)}.mp4`;
 
                 ctx.replyWithAnimation({ source: filePath });
                 return;
