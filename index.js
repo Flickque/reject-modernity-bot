3eded83e2b65a46e4ee73c3bb051afd3629dd65c
Flickque <flick86rus1@yandex.ru>
2024-06-16 16:05:46 +0400

    initial commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const { Telegraf } = require('telegraf');
+const dotenv = require('dotenv');
+
+dotenv.config();
+const bot = new Telegraf(process.env.TELEGRAM_BOT_API_KEY);
+
+const dictionaryMap = new Map([
+    ['понедельник', 'monday'],
+    ['вторник', 'tuesday'],
+    ['среда', 'wednesday'],
+    ['четверг', 'thursday'],
+    ['пятница', 'friday'],
+    ['суббота', 'saturday'],
+    ['воскресенье', 'sunday'],
+    ['праздник', 'holiday'],
+]);
+
+bot.on('message', (ctx) => {
+    const message = ctx.message.text ? ctx?.message?.text?.toLowerCase() : ctx?.message?.caption?.toLowerCase();
+    const msg = message ?? '';
+
+    for (let [key, value] of dictionaryMap.entries()) {
+        if (msg.includes(key)) {
+            try{
+                const filePath = `public/videos/${value}-1.mp4`;
+
+                ctx.replyWithAnimation({ source: filePath });
+                return;
+            } catch (e) {
+                ctx.reply('Something went wrong...')
+            }
+
+        }
+    }
+});
+
+
+
+bot.launch();
+
--- src/conts.mjs
+++
@@ -1,1 +0,0 @@
-export const basicGifPath = '../public/videos'
